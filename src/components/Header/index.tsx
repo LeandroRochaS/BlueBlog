98c889268cdbe3205aca4a79047d2bf4def79a80
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-02 23:07:18 -0300

    feat: update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,41 @@
 import "./styles.scss";
+import logoblog from "../../images/svg/logo.svg";
 
 export default function Header() {
+  const descLogo = "Essa é a logo do blog";
+
   return (
     <>
-      <h1>Sarve</h1>
+      <header className="flex-space-between">
+        <div className="logo">
+          <img src={logoblog} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input
+            type="text"
+            className="input-search"
+            placeholder="Pesquisar"
+            name="search"
+          />
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#" className="nav-link">
+              Categories
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
     </>
   );
 }
