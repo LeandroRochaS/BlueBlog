ae4ef0846349d7c43c48e727bcc4df76c77d1391
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-04 14:46:46 -0300

    feat: update style the posts

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -4,8 +4,59 @@ import "./styles.scss";
 export default function Article() {
   return (
     <>
-      <h1>Article</h1>
-      <Post />
+      <section className="container">
+        <h1>Article</h1>
+        <div className="mt-5 posts-container">
+          <Post
+            className="item-1"
+            imgProfile="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVzc29hfGVufDB8fDB8fHww"
+            img="https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            subtitle="tecnologia"
+            title="O guia definitivo sobre o blog"
+          >
+            1Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            nulla dolor eius fuga esse explicabo dolore, veritatis voluptates
+            dolorem corporis? Cumque numquam iure blanditiis, repellat
+            voluptatibus suscipit adipisci veritatis aperiam.
+          </Post>
+          <Post
+            className="item-2"
+            imgProfile="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVzc29hfGVufDB8fDB8fHww"
+            img="https://images.unsplash.com/photo-1618249608049-bce3784b6a4b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            subtitle="fotografia"
+            title="Quais melhores câmeras em 2021?"
+          >
+            2Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            nulla dolor eius fuga esse explicabo dolore, veritatis voluptates
+            dolorem corporis? Cumque numquam iure blanditiis, repellat
+            voluptatibus suscipit adipisci veritatis aperiam.
+          </Post>
+          <Post
+            className="item-3"
+            imgProfile="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVzc29hfGVufDB8fDB8fHww"
+            img="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            subtitle="cinema"
+            title="Os 10 filmes com maior bilheteria"
+          >
+            3Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            nulla dolor eius fuga esse explicabo dolore, veritatis voluptates
+            dolorem corporis? Cumque numquam iure blanditiis, repellat
+            voluptatibus suscipit adipisci veritatis aperiam.
+          </Post>
+          <Post
+            className="item-4"
+            imgProfile="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVzc29hfGVufDB8fDB8fHww"
+            img="https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            subtitle="tecnologia"
+            title="O guia definitivo sobre o blog"
+          >
+            1Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            nulla dolor eius fuga esse explicabo dolore, veritatis voluptates
+            dolorem corporis? Cumque numquam iure blanditiis, repellat
+            voluptatibus suscipit adipisci veritatis aperiam.
+          </Post>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,9 +1,45 @@
+import { ReactNode } from "react";
 import "./styles.scss";
 
-export default function Post() {
+type PostProps = {
+  children: ReactNode;
+  subtitle: string;
+  title: string;
+  img: string;
+  imgProfile: string;
+  className?: string;
+};
+
+export default function Post(props: PostProps) {
+  console.log(props.className);
   return (
     <>
-      <h1>Post</h1>
+      <div
+        className={`post mr-2 ${props.className}`}
+        style={{ backgroundImage: `url(${props.img})` }}
+      >
+        <div className="post-opacity">
+          <div className="img-post"></div>
+          <h5>{props.subtitle}</h5>
+          <h3>{props.title}</h3>
+          {props.className === "item-1" && (
+            <p className=" mt-1">{props.children}</p>
+          )}
+
+          <div className="flex mt-1">
+            <div className="img-profile">
+              <div
+                className="img-profile-content"
+                style={{ backgroundImage: `url(${props.imgProfile})` }}
+              ></div>
+            </div>
+            <div className="desc-profile ml-2">
+              <h6 className="color-blue">Fulano de tal</h6>
+              <p>Aug 2, 2020 - 8 min read</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
