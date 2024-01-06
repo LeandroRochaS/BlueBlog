ecc4adbb65cb7c472200c64a752bee53c90d620c
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-06 11:34:57 -0300

    :sparkles: feat: created component footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,23 +8,26 @@ import iconyoutube from "../../images/svg/icon-youtube.svg";
 export default function Footer() {
   return (
     <>
-      <footer className=" mt-7">
-        <div className="footer-title">
+      <footer className="mt-8">
+        <div className="footer-title mb-5">
           <img src={logo} alt="logo" className="mr-2" />
           <h4>
             blog <span className="pointer">.</span>
           </h4>
         </div>
-        <div className="footer-mid flex-space-between mt-4">
-          <div className="footer-left flex-space-between">
-            <div className="footer-mid-mid">
+        <div className="row">
+          <div className="grid-3 mt-4">
+            <div className="flex-column-start " style={{ gap: "20px" }}>
               <h4>Posts</h4>
               <p> Mais vistos</p>
               <p> Mais comentados</p>
               <p> Mais populares</p>
-              <p> Mais recentes</p>
+              <p> Mas recentes</p>
             </div>
-            <div className="footer-mid-mid ">
+          </div>
+
+          <div className="grid-2">
+            <div className="flex-column-start" style={{ gap: "20px" }}>
               <h4>Categorias</h4>
               <p> Tecnologia</p>
               <p> Fotografia</p>
@@ -32,31 +35,34 @@ export default function Footer() {
               <p> Esportes</p>
             </div>
           </div>
+          <div className="grid-1"></div>
+          <div className="grid-6 px-5 flex-column-start">
+            <div className="row">
+              <h4>Quer ser avisado dos novos posts do blog </h4>
 
-          <div className="footer-mid-right">
-            <h4>Quer ser avisado dos novos posts do blog? </h4>
-
-            <div>
-              <input
-                type="text"
-                className="footer-input"
-                placeholder="Digite seu e-mail"
-              />
-              <button>Enviar</button>
+              <div className="flex mt-2">
+                <input
+                  type="text"
+                  className="w-100"
+                  placeholder="Digite seu e-mail"
+                />
+                <button className="btn b-0 ml-1 w-50">Inscrever-se</button>
+              </div>
             </div>
           </div>
         </div>
+
         <hr className="mt-5"></hr>
 
-        <div className="footer-last flex-space-between mt-5">
+        <div className="footer-last flex-space-between mt-5 mb-5">
           <p className="h5Gray5">
             © 2024 | Blog. Todos os direitos reservados.
           </p>
           <div className="footer-icons flex-space-between">
-            <img src={iconfacebook} alt="icon-facebook" />
-            <img src={iconinstagram} alt="icon-instagram" />
-            <img src={icontwitter} alt="icon-twitter" />
-            <img src={iconyoutube} alt="icon-youtube" />
+            <img src={iconfacebook} className="icon-s" alt="icon-facebook" />
+            <img src={iconinstagram} className="icon-s" alt="icon-instagram" />
+            <img src={icontwitter} className="icon-s" alt="icon-twitter" />
+            <img src={iconyoutube} className="icon-s" alt="icon-youtube" />
           </div>
         </div>
       </footer>
