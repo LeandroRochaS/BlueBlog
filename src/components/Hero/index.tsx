85be7854f77078faef5dfb494c265cc2438e9f08
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-06 11:35:59 -0300

    :sparkles: feat: created hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,29 @@
+import "./styles.scss";
+import blogLogo from "../../images/svg/blog.svg";
+
+export default function Hero() {
+  return (
+    <>
+      <section className="hero mt-7">
+        <div className="row">
+          <div className="grid-6 ">
+            <h1 className="h0">
+              Blog<span>.</span>
+            </h1>
+            <p className="mt-1">
+              Um blog para todos. Escrever. Ler. Comentar. Contribuir. Aprender.
+              Conectar.
+            </p>
+            <a href="" className="btn mt-4">
+              {" "}
+              Começar a escrever
+            </a>
+          </div>
+          <div className="grid-6 ">
+            <img src={blogLogo} alt=" Logo do blog" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
