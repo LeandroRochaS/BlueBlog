84a7e407591bccfe2ecbac9110536a72645ee83f
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-06 11:35:32 -0300

    :sparkles: feat: created component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import "./styles.scss";
 import logoblog from "../../images/svg/logo.svg";
 import buscaSvg from "../../images/svg/buscainput.svg";
+import { useState } from "react";
 
 export default function Header() {
+  const [menuMobile, setMenuMobile] = useState(false);
+
   const descLogo = "Essa é a logo do blog";
+
+  function handleMenuMobile() {
+    setMenuMobile(!menuMobile);
+  }
 
   return (
     <>
@@ -17,11 +24,6 @@ export default function Header() {
           <ul className="menu">
             <li>
               <a href="#" className="nav-link">
-                Categories
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link">
                 About
               </a>
             </li>
@@ -32,30 +34,68 @@ export default function Header() {
             </li>
           </ul>
         </div>
-        <div className="bx"></div>
+        <div
+          onClick={handleMenuMobile}
+          className={`bx ${menuMobile ? "active" : ""}`}
+        ></div>
 
         <div className="flex-space-between ">
-          <form className="search mr-5">
-            <input
-              type="text"
-              className="input-search"
-              placeholder="Buscar"
-              name="search"
-            />
-            <img src={buscaSvg} alt="icone de lupa" />
-          </form>
+          <div className="search-container">
+            <form className="search mr-5">
+              <input
+                type="text"
+                className="input-search"
+                placeholder="Buscar"
+                name="search"
+              />
+              <img src={buscaSvg} alt="icone de lupa" />
+            </form>
+          </div>
           <div className="cta-desktop">
             <a className="btn" href="">
               Login
             </a>
           </div>
-          <div className="cta-mobile">
-            <a className="link" href="">
+          <div className="cta-mobile ">
+            <a className="link color-primary" href="">
               Login
             </a>
           </div>
         </div>
       </header>
+      {/*  MENU MOBILE   */}
+
+      <div className="relative">
+        <div
+          className={`menu-mobile ${
+            menuMobile ? "menu-mobile-opened" : "menu-mobile-closed"
+          }`}
+        >
+          <ul>
+            <li>
+              <a href="#" className="link-menu-mobile">
+                Sobre
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link-menu-mobile">
+                Contato
+              </a>
+            </li>
+            <li className="py-2 px-2">
+              <form className="search w-100 mr-5">
+                <input
+                  type="text"
+                  className="input-search"
+                  placeholder="Buscar"
+                  name="search"
+                />
+                <img src={buscaSvg} alt="icone de lupa" />
+              </form>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
