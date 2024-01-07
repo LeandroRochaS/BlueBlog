947cc7fded1a1602123121e200b421fa0df2b65a
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-07 10:39:23 -0300

    :sparkles: feat: configured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<Post />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
