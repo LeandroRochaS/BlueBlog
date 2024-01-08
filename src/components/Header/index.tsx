c059e8547e67670a5a970829de438ae6e9c3c994
Leandro Rocha Santos <lerocha644@gmail.com>
2024-01-07 23:04:45 -0300

    :lipstick: feat: config links and search forms

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,76 +2,114 @@ import "./styles.scss";
 import logoblog from "../../images/svg/logo.svg";
 import buscaSvg from "../../images/svg/buscainput.svg";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+
+const initialValueForm = {
+  search: "",
+};
 
 export default function Header() {
   const [menuMobile, setMenuMobile] = useState(false);
-
-  const descLogo = "Essa é a logo do blog";
+  const [form, setForm] = useState(initialValueForm);
+  const navigate = useNavigate();
+  const { isLoggedUser, userDataAuthContext } = useAuthContext();
 
   function handleMenuMobile() {
     setMenuMobile(!menuMobile);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function onChange(e: any) {
+    const { value, name } = e.target;
+    setForm({ ...form, [name]: value });
+    console.log(form);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSearch(e: any) {
+    e.preventDefault();
+
+    navigate(`/search/${form.search}`);
+  }
+
   return (
     <>
-      <header className="flex-space-between">
-        <div className="flex-space-between">
-          <div className="logo">
-            <Link to={"/"}>
-              <img src={logoblog} alt={descLogo} />
-            </Link>
+      <header className="">
+        <div className="flex-space-between header">
+          <div className="flex-space-between">
+            <div className="logo">
+              <Link to={"/"}>
+                <img src={logoblog} alt={"logo do blog"} />
+              </Link>
+            </div>
+            <ul className="menu">
+              <li>
+                <Link to={"/about"} className="nav-link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to={"/contact"} className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
           </div>
-          <ul className="menu">
-            <li>
-              <Link to={"/about"} className="nav-link">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to={"/contact"} className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div
-          onClick={handleMenuMobile}
-          className={`bx ${menuMobile ? "active" : ""}`}
-        ></div>
+          <div
+            onClick={handleMenuMobile}
+            className={`bx ${menuMobile ? "active" : ""}`}
+          ></div>
 
-        <div className="flex-space-between ">
-          <div className="search-container">
-            <form className="search mr-5">
-              <input
-                type="text"
-                className="input-search"
-                placeholder="Buscar"
-                name="search"
-              />
-              <img src={buscaSvg} alt="icone de lupa" />
-            </form>
-          </div>
-          <div className="cta-desktop">
-            <a className="btn" href="">
-              Login
-            </a>
-          </div>
-          <div className="cta-mobile ">
-            <a className="link color-primary" href="">
-              Login
-            </a>
+          <div className="flex-space-between ">
+            <div className="search-container">
+              <form className="search mr-5">
+                <input
+                  type="text"
+                  className=""
+                  placeholder="Buscar"
+                  name="search"
+                  onChange={(e) => onChange(e)}
+                  autoComplete="off"
+                />
+                <img
+                  src={buscaSvg}
+                  onClick={(e) => handleSearch(e)}
+                  alt="icone de lupa"
+                />
+              </form>
+            </div>
+            {!isLoggedUser && !userDataAuthContext ? (
+              <>
+                <div className="cta-desktop">
+                  <Link to={"/login"} className="btn">
+                    Login
+                  </Link>
+                </div>
+                <div className="cta-mobile ">
+                  <Link to={"/login"} className="btn ">
+                    Login
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <div className="cta-desktop flex-aling">
+                <Link to={"/profile"} className=" ">
+                  <img
+                    src={userDataAuthContext?.ImageProfile}
+                    alt="foto de perfil"
+                    className="profile b-primary mr-2"
+                  />
+                </Link>
+                <p className="color-white">Olá, {userDataAuthContext?.name}</p>
+              </div>
+            )}
           </div>
         </div>
       </header>
-      {/*  MENU MOBILE   */}
 
-      <div className="relative">
-        <div
-          className={`menu-mobile ${
-            menuMobile ? "menu-mobile-opened" : "menu-mobile-closed"
-          }`}
-        >
+      <div className="relative mb-12">
+        <div className={`menu-mobile ${menuMobile ? "showmenu" : ""}`}>
           <ul>
             <li>
               <Link to={"/about"} className="link-menu-mobile">
@@ -84,14 +122,19 @@ export default function Header() {
               </Link>
             </li>
             <li className="py-2 px-2">
-              <form className="search w-100 mr-5">
+              <form className="search mr-5">
                 <input
                   type="text"
-                  className="input-search"
+                  className=""
                   placeholder="Buscar"
                   name="search"
+                  onChange={(e) => onChange(e)}
                 />
-                <img src={buscaSvg} alt="icone de lupa" />
+                <img
+                  src={buscaSvg}
+                  alt="icone de lupa"
+                  onClick={(e) => handleSearch(e)}
+                />
               </form>
             </li>
           </ul>
